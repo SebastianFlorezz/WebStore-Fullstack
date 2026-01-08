47a591f48740f885d222d2b0b6409276e416e036
sebastianflorezz <sebastianflorezbuelvas@gmail.com>
2026-01-08 03:33:11 -0500

    updateUserName route

--- backend/src/controllers/user/routes.js
+++ backend/src/controllers/user/routes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login} = require("./controller");
+const { register, login, updateUserName} = require("./controller");
 
 router.post("/register", register);
 router.post("/login", login);
-
-
+router.put("/updateUserName", updateUserName);
 
 
 
